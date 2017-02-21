1e7954a4c57378cf4cc82d459ee24c9a86e84894
Ap0c <Ap0c@users.noreply.github.com>
2017-02-21 21:49:59 +0000

    Migrated main site module over to flask.

---
+++ run.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=site.py
+flask run
--- site.py
+++ site.py
@@ -1,20 +1,14 @@
 # ----- Imports ----- #
 
-import os
+from flask import Flask, render_template
+
 import db
 import scoring
-import bottle
-from bottle import Bottle, view, static_file, run
 
 
 # ----- Setup ----- #
 
-CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-STATIC_PATH = CURRENT_DIR + '/site/static'
-
-app = Bottle()
-bottle.TEMPLATE_PATH.insert(0, CURRENT_DIR + '/site/views')
-
+app = Flask(__name__, static_url_path='/static')
 
 
 # ----- Functions ----- #
@@ -39,30 +33,20 @@ def split_races(results):
 # ----- Routes ----- #
 
 @app.route('/2014')
-@view('index.html')
 def index():
+
 	query = 'SELECT * FROM results where year > 2014'
 	results = db.c().execute(query).fetchall()
 	races = split_races(results)
 
-	return dict(races=races)
+	return render_template('index.html', races=races)
+
 
 @app.route('/prices')
-@view('prices.html')
 def prices():
+
 	query = 'SELECT DISTINCT driver FROM results WHERE year = 2014'
 	drivers = db.c().execute(query).fetchall()
 	driver_prices = [(d[0], scoring.price(d[0])) for d in drivers]
-	return dict(prices=driver_prices)
 
-@app.route('/static/<filepath:path>')
-def load_static(filepath):
-
-	return static_file(filepath, root=STATIC_PATH)
-
-
-# ----- Run ----- #
-
-if __name__ == '__main__':
-
-	run(app, host='localhost', port=8080)
+	return render_template('prices.html', prices=driver_prices)
